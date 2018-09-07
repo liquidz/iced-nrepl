f61fb83e667176ab5756eab2b6574abc04b651a4
liquidz <liquidz.uo@gmail.com>
2018-09-07 23:19:03 +0000

    Update dependencies

--- project.clj
+++ project.clj
@@ -1,17 +1,18 @@
 (defproject iced-nrepl "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "nREPL middleware for vim-iced"
+  :url "https://github.com/liquidz/iced-nrepl"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [clj-http "3.9.1"]
-                 [jonase/eastwood "0.2.9" :exclusions [org.clojure/clojure]]
-                 [nrepl "0.4.5"]
                  [cider/orchard "0.3.0"]
+                 [clj-http "3.9.1"]
                  [cljfmt "0.6.0"]
-                 ;;[cider/piggieback "0.3.9"]
-                 [refactor-nrepl "2.4.0"]
-                 [cider/cider-nrepl "0.18.0"]]
+                 [jonase/eastwood "0.2.9" :exclusions [org.clojure/clojure]]
+                 [nrepl "0.4.5"]]
   :profiles
   {:dev
-   {:dependencies [[fudje "0.9.7"]]}})
+   {:dependencies [[cider/cider-nrepl "0.18.0"]
+                   [cider/piggieback "0.3.8"]
+                   [fudje "0.9.7"]
+                   [leiningen-core "2.7.1"]
+                   [refactor-nrepl "2.4.0"]]}})
