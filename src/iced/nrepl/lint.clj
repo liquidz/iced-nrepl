d47e128035be0efa5822fcb6d86c790c27974f1a
liquidz <liquidz.uo@gmail.com>
2018-09-10 16:06:13 +0900

    Add lint-file

--- src/iced/nrepl/lint.clj
+++ src/iced/nrepl/lint.clj
@@ -1,6 +1,10 @@
 (ns iced.nrepl.lint
-  (:require [clojure.set :as set]
-            [eastwood.lint :as el]))
+  (:require [clojure
+             [set :as set]
+             [string :as str]]
+            [clojure.java.io :as io]
+            [eastwood.lint :as el]
+            [iced.util.namespace :as i.u.ns]))
 
 (defn lint-by-eastwood [ns-sym linters]
   (let [linters (when (sequential? linters)
@@ -13,3 +17,27 @@
           (update :uri #(.getPath %))
           (set/rename-keys {:uri :path})))))
 
+(defn- remove-exception-name [s]
+  (str/replace s "java.lang.RuntimeException: " ""))
+
+(defn- extract-line-number [s]
+  (when-let [[[_ n]] (re-seq #"starting at line (\d+)" s)]
+      (Long/parseLong n)))
+
+(defn check-file-syntax [file-path]
+  (with-open [rdr (clojure.lang.LineNumberingPushbackReader. (io/reader file-path))]
+    (binding [*read-eval* false]
+      (try
+        (loop [result nil]
+          (when-not (= result :__EOF__)
+            (recur (read rdr false :__EOF__))))
+        (catch Exception ex
+          (let [msg (remove-exception-name (.getMessage ex))
+                line (or (extract-line-number msg) (.getLineNumber rdr))]
+            [{:msg msg :line line :column 0 :path file-path}]))))))
+
+(defn lint-file [file-path eastwood-linters]
+  (or (check-file-syntax file-path)
+      (some-> (slurp file-path)
+              i.u.ns/extract-ns-sym
+              (lint-by-eastwood eastwood-linters))))
--- test/iced/nrepl/lint_test.clj
+++ test/iced/nrepl/lint_test.clj
@@ -15,3 +15,22 @@
       (sut/lint-by-eastwood 'dummy nil)
       (fj/just [{:column 1 :line 2 :msg "dummy"
                  :path (fj/checker #(str/ends-with? % "project.clj"))}])))))
+
+(t/deftest check-file-syntax-test
+  (let [path "test/files/lint/not_closing_paren.edn"]
+    (t/is
+     (compatible
+      (sut/check-file-syntax path)
+      (fj/just [{:path path
+                 :msg (fj/checker string?)
+                 :line 3
+                 :column 0}]))))
+
+  (let [path "test/files/lint/not_matched_closing_paren.edn"]
+    (t/is
+     (compatible
+      (sut/check-file-syntax path)
+      (fj/just [{:path path
+                 :msg (fj/checker string?)
+                 :line 6
+                 :column 0}])))))
