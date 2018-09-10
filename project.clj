34364553c05dd368c36e911804405e72648d3424
liquidz <liquidz.uo@gmail.com>
2018-09-10 16:04:11 +0900

    Tweak project.clj

--- project.clj
+++ project.clj
@@ -6,13 +6,12 @@
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [cider/orchard "0.3.0"]
                  [clj-http "3.9.1"]
-                 [cljfmt "0.6.0"]
+                 [cljfmt "0.6.0" :exclusions [org.clojure/tools.reader]]
                  [jonase/eastwood "0.2.9" :exclusions [org.clojure/clojure]]
                  [nrepl "0.4.5"]]
   :profiles
   {:dev
    {:dependencies [[cider/cider-nrepl "0.18.0"]
-                   [cider/piggieback "0.3.8"]
                    [fudje "0.9.7"]
                    [leiningen-core "2.7.1"]
                    [refactor-nrepl "2.4.0"]]}})
