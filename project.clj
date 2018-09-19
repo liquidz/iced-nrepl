3a8cd53ba28db210f7ee68c569ad11c79cceaabe
liquidz <liquidz.uo@gmail.com>
2018-09-19 18:00:11 +0900

    Bump dependencies version

--- project.clj
+++ project.clj
@@ -4,9 +4,9 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [cider/orchard "0.3.0"]
+                 [cider/orchard "0.3.1"]
                  [clj-http "3.9.1"]
-                 [cljfmt "0.6.0" :exclusions [org.clojure/tools.reader]]
+                 [cljfmt "0.6.1" :exclusions [org.clojure/tools.reader]]
                  [jonase/eastwood "0.2.9" :exclusions [org.clojure/clojure]]
                  [medley "1.0.0"]
                  [nrepl "0.4.5"]]
@@ -14,7 +14,7 @@
   {:dev
    {:dependencies [[cider/cider-nrepl "0.18.0"]
                    [fudje "0.9.7"]
-                   [leiningen-core "2.7.1"]
+                   [leiningen-core "2.8.1"]
                    [refactor-nrepl "2.4.0"]]}}
 
   :release-tasks [["vcs" "assert-committed"]
