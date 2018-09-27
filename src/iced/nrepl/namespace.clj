56bc709ea02a4e128c14e47ca40ae38118dabea4
liquidz <liquidz.uo@gmail.com>
2018-09-27 17:07:15 +0900

    Add function to fetch ns-aliases

--- src/iced/nrepl/namespace.clj
+++ src/iced/nrepl/namespace.clj
@@ -1,5 +1,8 @@
 (ns iced.nrepl.namespace
-  (:require [orchard.namespace :as o.ns]))
+  (:require [iced.util.namespace :as i.u.ns]
+            [medley.core :as medley]
+            [orchard.namespace :as o.ns]
+            [refactor-nrepl.ns.ns-parser :as r.ns.parser]))
 
 (defn project-namespaces [& [prefix]]
   (let [ls (-> (o.ns/project-namespaces)
@@ -7,3 +10,18 @@
                distinct)]
     (cond->> ls
       prefix (filter #(.startsWith (str %) prefix)))))
+
+(defn- ensure-string-map [m]
+  (medley/map-kv (fn [k v] [(str k) (str v)]) m))
+
+(defmulti aliases (fn [env _] env))
+(defmethod aliases "clj"
+  [_ ns-code]
+  (->> ns-code i.u.ns/extract-ns-sym ns-aliases
+       (medley/map-vals ns-name)
+       ensure-string-map))
+
+(defmethod aliases "cljs"
+  [_ ns-code]
+  (-> ns-code read-string r.ns.parser/get-libspecs r.ns.parser/aliases
+      ensure-string-map))
--- test/iced/nrepl/namespace_test.clj
+++ test/iced/nrepl/namespace_test.clj
@@ -14,3 +14,14 @@
   (let [res (set (sut/project-namespaces "iced.util"))]
     (t/is (contains? res 'iced.util.namespace))
     (t/is (not (contains? res 'iced.nrepl.namespace)))))
+
+(t/deftest aliases-test
+  (t/testing "clj"
+    (t/is (= {"t" "clojure.test"
+              "fj" "fudje.sweet"
+              "sut" "iced.nrepl.namespace"}
+             (sut/aliases "clj" "(ns iced.nrepl.namespace-test)"))))
+
+  (t/testing "cljs"
+    (t/is (= {"bar" "foo.bar"}
+             (sut/aliases "cljs" "(ns foo.core (:require [foo.bar :as bar]))")))))
