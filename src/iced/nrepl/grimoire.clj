6675ce188030f82378feeac0e59bc30222f986c3
liquidz <liquidz.uo@gmail.com>
2018-11-28 17:36:16 +0900

    Fix grimoire query to encode

--- src/iced/nrepl/grimoire.clj
+++ src/iced/nrepl/grimoire.clj
@@ -1,8 +1,11 @@
 (ns iced.nrepl.grimoire
-  (:require [clj-http.client :as client]))
+  (:require [clj-http.client :as client]
+            [clj-http.util :as util]))
 
 (def ^:private grimoire-url "https://conj.io")
 
 (defn search [platform ns-name sym]
-  (let [url (format "%s/search/v1/%s/%s/%s/" grimoire-url platform ns-name sym)]
+  (let [url (format "%s/search/v1/%s/%s/%s/"
+                    grimoire-url platform ns-name
+                    (util/url-encode sym))]
     (client/get url {:content-type :text})))
