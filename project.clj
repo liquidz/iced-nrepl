275451341b275af9bd1a0d530fa2a9dc3160ab89
liquidz <liquidz.uo@gmail.com>
2018-11-28 17:42:37 +0900

    Bump clojur 1.10 to RC2

--- project.clj
+++ project.clj
@@ -17,7 +17,7 @@
   :profiles
   {:dev {:dependencies [[fudje "0.9.7"]]}
    :1.9 [:dev {:dependencies [[org.clojure/clojure "1.9.0"]]}]
-   :1.10 [:dev {:dependencies [[org.clojure/clojure "1.10.0-beta4"]]}]}
+   :1.10 [:dev {:dependencies [[org.clojure/clojure "1.10.0-RC2"]]}]}
 
   :release-tasks [["vcs" "assert-committed"]
                   ["change" "version" "leiningen.release/bump-version" "release"]
