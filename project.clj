5577b54dda4696fe54562b112fda83b7d755683d
liquidz <liquidz.uo@gmail.com>
2018-12-18 14:37:24 +0900

    Update project.clj to use mranderson

--- project.clj
+++ project.clj
@@ -3,21 +3,30 @@
   :url "https://github.com/liquidz/iced-nrepl"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.clojure/data.json "0.2.6"]
-                 [org.clojure/test.check "0.10.0-alpha3"]
-                 [cider/orchard "0.3.3"]
-                 [clj-http "3.9.1"]
-                 [cljfmt "0.6.2" :exclusions [org.clojure/tools.reader
-                                              org.clojure/clojurescript]]
-                 [jonase/eastwood "0.3.4" :exclusions [org.clojure/clojure]]
-                 [medley "1.0.0"]
+  :dependencies [[org.clojure/clojure "1.10.0"]
                  [nrepl "0.5.3"]
-                 [refactor-nrepl "2.4.0"]]
+                 [refactor-nrepl "2.4.0"]
+
+                 ^:source-dep [org.clojure/data.json "0.2.6"]
+                 ^:source-dep [org.clojure/test.check "0.10.0-alpha3"]
+                 ^:source-dep [org.clojure/tools.namespace "0.3.0-alpha4"]
+                 ^:source-dep [cider/orchard "0.3.3"]
+                 ^:source-dep [cljfmt "0.6.3" :exclusions [org.clojure/clojurescript
+                                                           org.clojure/tools.reader]]
+                 ^:source-dep [http-kit "2.3.0"]
+                 ^:source-dep [jonase/eastwood "0.3.4"
+                               :exclusions [org.clojars.brenton/google-diff-match-patch
+                                            org.clojure/clojure]]
+                 ^:source-dep [medley "1.0.0"]]
+
+  :java-source-paths [".diff-match-patch/java/src"]
+
+  :plugins [[thomasa/mranderson "0.4.9"]]
+
   :profiles
   {:dev {:dependencies [[fudje "0.9.7"]]}
    :1.9 [:dev {:dependencies [[org.clojure/clojure "1.9.0"]]}]
-   :1.10 [:dev {:dependencies [[org.clojure/clojure "1.10.0-RC3"]]}]}
+   :1.10 [:dev {:dependencies [[org.clojure/clojure "1.10.0"]]}]}
 
   :release-tasks [["vcs" "assert-committed"]
                   ["change" "version" "leiningen.release/bump-version" "release"]
