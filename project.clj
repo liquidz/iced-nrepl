f1e324ab5967c937377179ff13f08a6afcea13c4
liquidz <liquidz.uo@gmail.com>
2019-01-15 17:33:59 +0900

    Version 0.4.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject iced-nrepl "0.4.0-SNAPSHOT"
+(defproject iced-nrepl "0.4.0"
   :description "nREPL middleware for vim-iced"
   :url "https://github.com/liquidz/iced-nrepl"
   :license {:name "Eclipse Public License"
