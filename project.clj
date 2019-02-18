0221d16d0af9171d172150c89d12c1738d8dd2be
liquidz <liquidz.uo@gmail.com>
2019-02-18 12:57:47 +0900

    Bump nrepl to 0.6.0

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/liquidz/iced-nrepl"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[nrepl "0.5.3"]
+  :dependencies [[nrepl "0.6.0"]
                  [refactor-nrepl "2.4.0"]
 
                  ^:source-dep [org.clojure/data.json "0.2.6"]
