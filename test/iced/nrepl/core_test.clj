ce1cf74762d6db1532c0af140944908a08e6e205
liquidz <liquidz.uo@gmail.com>
2019-06-24 01:36:21 +0900

    Add iced.nrepl.core-test

---
+++ test/iced/nrepl/core_test.clj
@@ -0,0 +1,10 @@
+(ns iced.nrepl.core-test
+  (:require [clojure.test :as t]
+            [iced.test-helper :as h]))
+
+(t/use-fixtures :once h/repl-server-fixture)
+
+(t/deftest version-test
+  (let [resp (h/message {:op "iced-version"})]
+    (t/is (contains? (:status resp) "done"))
+    (t/is (string? (:version resp)))))
