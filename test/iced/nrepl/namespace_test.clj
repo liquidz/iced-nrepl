1ff6476b290329bc6c9884b749f880268c878c18
liquidz <liquidz.uo@gmail.com>
2019-06-24 00:32:48 +0900

    Update nrepl.namespace tests to use test-helper

--- test/iced/nrepl/namespace_test.clj
+++ test/iced/nrepl/namespace_test.clj
@@ -1,5 +1,11 @@
 (ns iced.nrepl.namespace-test
   (:require [clojure.test :as t]
-            [iced.nrepl.namespace :as sut]))
+            [iced.test-helper :as h]))
 
+(t/use-fixtures :once h/repl-server-fixture)
 
+(t/deftest project-ns-list-test
+  (t/testing "iced-project-ns-list"
+    (let [resp (h/message {:op "iced-project-ns-list"})]
+      (t/is (contains? (:status resp) "done"))
+      (t/is (sequential? (:project-ns-list resp))))))
