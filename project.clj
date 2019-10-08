ced44063492895ae1b74534d82f6c21a9adf26e6
liquidz <liquidz.uo@gmail.com>
2019-10-09 07:23:17 +0900

    Bump orchard to 0.5.3

--- project.clj
+++ project.clj
@@ -10,7 +10,7 @@
                                :exclusions [org.clojure/clojure]]
                  ^:inline-dep [org.clojure/tools.namespace "0.3.1" ; required by cljfmt
                                :exclusions [org.clojure/java.classpath]]
-                 ^:inline-dep [cider/orchard "0.5.2"]
+                 ^:inline-dep [cider/orchard "0.5.3"]
                  ^:inline-dep [cljfmt "0.6.4"
                                :exclusions [org.clojure/clojure
                                             org.clojure/clojurescript
