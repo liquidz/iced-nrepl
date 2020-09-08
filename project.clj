06d67346f2db6a55d5a75c2eb3026d5e42a89aa6
liquidz <liquidz.uo@gmail.com>
2020-09-09 06:25:47 +0900

    Bump cljfmt

--- project.clj
+++ project.clj
@@ -11,7 +11,7 @@
                  ^:inline-dep [org.clojure/tools.namespace "1.0.0" ; required by cljfmt
                                :exclusions [org.clojure/java.classpath]]
                  ^:inline-dep [cider/orchard "0.6.0"]
-                 ^:inline-dep [cljfmt "0.6.8"
+                 ^:inline-dep [cljfmt "0.7.0"
                                :exclusions [org.clojure/clojure
                                             org.clojure/clojurescript
                                             org.clojure/tools.reader]]
