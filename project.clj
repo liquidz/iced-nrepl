66d82eb9a3dc53be6dee405821374187d33a628f
liquidz <liquidz.uo@gmail.com>
2021-01-05 06:58:46 +0900

    Bump orchard to 0.6.2

--- project.clj
+++ project.clj
@@ -10,7 +10,7 @@
                                :exclusions [org.clojure/clojure]]
                  ^:inline-dep [org.clojure/tools.namespace "1.1.0" ; required by cljfmt
                                :exclusions [org.clojure/java.classpath]]
-                 ^:inline-dep [cider/orchard "0.6.1"]
+                 ^:inline-dep [cider/orchard "0.6.2"]
                  ;; TODO cljfmt 0.7.0 seems to be failed
                  ^:inline-dep [cljfmt "0.6.8"
                                :exclusions [org.clojure/clojure
