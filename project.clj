c1a392086f783fce07768ab145060366b1754e9b
liquidz <liquidz.uo@gmail.com>
2021-01-22 06:33:50 +0900

    Bump cljfmt to 0.7.0

--- project.clj
+++ project.clj
@@ -11,8 +11,7 @@
                  ^:inline-dep [org.clojure/tools.namespace "1.1.0" ; required by cljfmt
                                :exclusions [org.clojure/java.classpath]]
                  ^:inline-dep [cider/orchard "0.6.2"]
-                 ;; TODO cljfmt 0.7.0 seems to be failed
-                 ^:inline-dep [cljfmt "0.6.8"
+                 ^:inline-dep [cljfmt "0.7.0"
                                :exclusions [org.clojure/clojure
                                             org.clojure/clojurescript
                                             org.clojure/tools.reader]]
