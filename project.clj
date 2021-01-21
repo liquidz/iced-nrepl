80edd3530e7f5a998bac143df0dd63896ff4e46a
liquidz <liquidz.uo@gmail.com>
2021-01-22 06:32:29 +0900

    Bump mranderson to 0.5.3-SNAPSHOT

--- project.clj
+++ project.clj
@@ -19,8 +19,8 @@
                  ^:inline-dep [medley "1.3.0"
                                :exclusions [org.clojure/clojure]]]
 
-  :plugins [[thomasa/mranderson "0.5.2"]
             [lein-cloverage "1.2.1"]]
+  :plugins [[thomasa/mranderson "0.5.3-SNAPSHOT"]
   :mranderson {:project-prefix "mrandersonicednrepl"}
 
   :profiles
